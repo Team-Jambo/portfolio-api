3720cc28079d666951e290eac2f25f24a0fa9e3b
PriscaKing <priscyking3@gmail.com>
2024-07-16 15:03:00 +0000

    deleted sanitize in experience schema

--- schema/experience_schema.js
+++ schema/experience_schema.js
@@ -2,8 +2,8 @@ import joi from 'joi';
 
 const workexperienceSchema = joi.object({
    workExperience:{
-        position:joi.string().required().sanitize(),
-        company:joi.string().required().sanitize(),
+        position:joi.string().required(),
+        company:joi.string().required(),
         location:joi.string().default(null),
         startDate:joi.string().required(),
         endDate:joi.string().required(),
