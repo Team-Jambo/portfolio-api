97a71d75bb74a1a4cf85eaeeb9b47d21208e38bd
xeroxb4 <xeroxb4@gmail.com>
2024-07-16 15:02:23 +0000

    add

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,28 @@
 import jwt from "jsonwebtoken";
 
 
-export const checkUserSession = (req, res, next) =>{
+export const checkUserSession = (req, res, next) => {
 
     // check is session has a user
     if (req.session.user) {
         next()
-    } else if(req.headers.authorization){
+    } else if (req.headers.authorization) {
         // Extract token from headers
         const token = req.headers.authorization.split(' ')[1];
 
 
         // verify the token to get user
-const user = jwt.verify(token, precess.env.JWT_PRIVATE_KEY);
+        const user = jwt.verify(token, precess.env.JWT_PRIVATE_KEY);
 
         // Append user to request
+        req.user = user;
 
         // call next function
         res.json(token);
 
-     
-    }else{
+
+    } else {
         res.status(401).json('User not authenticated');
     }
-       
+
 }
