b33256957131bcc5d33168c1f915bcf2701b9091
Offet <nuellaemmanuella7003@gmail.com>
2024-07-16 10:15:16 +0000

    Swagger links added

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import { dbConnection } from "./config/db.js";
+import expressOasGenerator from "@mickeymond/express-oas-generator";
 
 //  instantiate express
 const app = express();
+
+//  for the swagger
+expressOasGenerator.handleResponses(app, {
+    alwaysServeDocs: true,
+    tags: ["Achievement", "User", "Profile", "Education", "Experience", "Volunteer", "Skills", "Project"],
+    mongooseModels: mongoose.modelNames()
+});
+
+//  user generator
+expressOasGenerator.handleRequests();
+app.use((req, res) => res.redirect("/api-docs"));
+
 
 // instantiate dbconnection
 dbConnection();
